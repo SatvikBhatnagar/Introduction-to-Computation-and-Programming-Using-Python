274fac96c185bbd891ce055fa90108911f69006f
SatvikBhatnagar <e19epy010@bennett.edu.in>
2023-02-15 01:48:37 +0530

    program complete

--- finger_exercise-1.1.py
+++ finger_exercise-1.1.py
@@ -11,15 +11,43 @@ Created on Thu Jan 19 01:15:56 2023
                        and prints the largest odd number among them. If none 
                        of them are odd, it should print a message to that effect. '''
                        
-x = 10
-y = 11
-z= 13
+x = 101
+y = 111
+z= 1341
 
 if x % 2 == 1:
-    print("x = ", x, "is odd")
+    if y % 2 == 1:
+        if z % 2 == 1:
+            if z > y:
+                if z > x:
+                    print(z, "is the largest odd")
+                else:
+                    print (x, "is the largest odd")
+            else:
+                if y > x:
+                    print(y, "is the largest odd")
+                else:
+                    print (x, "is the largest odd")
+        else:
+            if y > x:
+                print(y, "is the largest odd")
+            else:
+                print (x, "is the largest odd")
+    else:
+        print(x, "is the largest odd")
+            
+
 elif y % 2 == 1:
-    print("y = ", y, "is odd")
+    if z % 2 == 1:
+        if z > y:
+            print (z, "is the largest odd")
+        else:
+            print (y, "is the largest odd")
+    else:
+        print (z, "is the largest odd")
+        
 elif z % 2 == 1:
-    print("z = ", z, "is odd")
+    print (z, "is the largest odd")
+    
 else:
-    print("none of the numbers are odd")
+    print ("none of the numbers is an odd")
