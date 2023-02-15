4cf2dfa5e77704ce987e327ec8bf9bcc3a2630c1
SatvikBhatnagar <e19epy010@bennett.edu.in>
2023-02-15 10:22:40 +0530

    squaring the hard way using while loop

---
+++ p1_squaring_the_hard_way_pg22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 10:19:45 2023
+
+@author: stoner69
+"""
+
+#Square an integer, the hard way
+x = 5
+ans = 0
+itersLeft = x
+while (itersLeft != 0):
+    ans = ans + x
+    itersLeft = itersLeft - 1
+print(str(x) + '*' + str(x) + ' = ' + str(ans))
