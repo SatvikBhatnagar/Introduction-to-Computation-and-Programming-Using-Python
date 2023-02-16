09c438243f17bf27070d9ff4794ed36b47122403
SatvikBhatnagar <e19epy010@bennett.edu.in>
2023-02-17 01:40:37 +0530

    using 'in' operator

---
+++ finger_exercise-3.2_pg30.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 00:23:07 2023
+
+@author: stoner69
+"""
+
+"""Finger Exercise -- Let s be a string that contains a sequence of decimal 
+                        numbers separated by commas, e.g., s= '1.23,2.4,3.123'.
+                        Write a program that prints the sum of the numbers in s."""
+                        
+s = '1.23,234.1'
+total = 0
+temp = ''
+for i in s:
+    if i != ',':
+        temp = temp + i
+    elif i == ',':
+        if temp != '':
+            total = total + float(temp)
+            temp = ''        
+        else:
+            pass
+
+total = total + float(temp)        
+print(total)
+                    
