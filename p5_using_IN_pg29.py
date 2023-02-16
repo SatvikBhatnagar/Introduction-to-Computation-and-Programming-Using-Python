f4eee5018461f0c3e19b6bd31f38b1a2485851ba
SatvikBhatnagar <e19epy010@bennett.edu.in>
2023-02-17 00:22:17 +0530

    in operator

---
+++ p5_using_IN_pg29.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 00:19:38 2023
+
+@author: stoner69
+"""
+
+total = 0
+for c in '12345678':
+    total = total + int(c)
+print(total)
