7653a093578dc8254d452f8f604cc24a320dc683
SatvikBhatnagar <e19epy010@bennett.edu.in>
2023-02-17 01:42:35 +0530

    updated the program to work even if the string ends with ',' or '.'

--- finger_exercise-3.2_pg30.py
+++ finger_exercise-3.2_pg30.py
@@ -10,9 +10,13 @@ Created on Fri Feb 17 00:23:07 2023
                         numbers separated by commas, e.g., s= '1.23,2.4,3.123'.
                         Write a program that prints the sum of the numbers in s."""
                         
-s = '1.23,234.1'
+s = '1.23,234.1,.'
 total = 0
 temp = ''
+
+while s[-1] == ',' or s[-1] == '.':
+    s = s[0:len(s)-1]
+
 for i in s:
     if i != ',':
         temp = temp + i
